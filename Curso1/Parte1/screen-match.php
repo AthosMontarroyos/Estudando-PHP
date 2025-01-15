7c976bfcdcef1aa014017f46f09b98e63eddd8d2
AthosMontarroyos <155916624+AthosMontarroyos@users.noreply.github.com>
2025-01-15 19:11:10 -0300

    RE-ORGANIZADA NAS PASTAS

---
+++ Curso1/Parte1/screen-match.php
@@ -0,0 +1,14 @@
+<?php
+
+echo "Bem Vindo(a) ao Anime Match!
+";
+
+$nomeAnime = "Solo Leveling";
+$anoLancamento = 2024;
+$somaDeNotas = (5+3+2+5+4);
+$notaAnime = $somaDeNotas / 5;
+$incluidoNoPlano = true;
+
+echo $nomeAnime;
+echo $notaAnime;
+;
