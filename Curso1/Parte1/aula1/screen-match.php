2aad1c1a6085385cbb8041f5c993e9ced34d2ee9
AthosMontarroyos <155916624+AthosMontarroyos@users.noreply.github.com>
2025-01-15 18:29:43 -0300

    Fazendo Alterações

--- Curso1/Parte1/aula1/screen-match.php
+++ Curso1/Parte1/aula1/screen-match.php
@@ -1,3 +1,14 @@
 <?php
 
-echo "Bem Vindo(a) ao screen match!";
+echo "Bem Vindo(a) ao Anime Match!
+";
+
+$nomeAnime = "Solo Leveling";
+$anoLancamento = 2024;
+$avaliacoesAnime = 5+3+2+5+6;
+$quatidadeDeAvaliacoes = $avaliacoesAnime(
+$notaFinal = $notaAnime / $notaAnime.le
+$incluidoNoPlano = true;
+
+echo $nomeAnime
+;
