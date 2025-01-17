92ed15f21b1c450c1b774ec4e10d15ae30f069e3
AthosMontarroyos <155916624+AthosMontarroyos@users.noreply.github.com>
2025-01-16 23:40:43 -0300

    FIM EXERCICIOS AULA 3

--- Curso1/Parte1/exercicios/exercicioAula3.php
+++ Curso1/Parte1/exercicios/exercicioAula3.php
@@ -12,5 +12,13 @@ echo "\n$AlturaElevada";
 $IMS = $Peso/$AlturaElevada;
 echo "\n$IMS";
 
-$Horario = 18;
-if (
+$Hora = 13;
+if ($Hora >= 6 && $Hora < 12) {
+    echo "\nBom Dia";
+} else if($Hora >= 12 && $Hora < 18) {
+    echo "\nBoa Tarde";
+} else if ($Hora >= 18 && $Hora < 24) {
+    echo "\nBoa Noite";
+} else {
+    echo "\nBoa Madrugada";
+}
